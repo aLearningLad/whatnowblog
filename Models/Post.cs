c15a5d55a0d824b87347c0748d693ed7952dd571
aLearningLad <mocmanca@gmail.com>
2024-12-12 22:56:44 +0200

    Add project files.

---
+++ Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace whatnowblog.Models
+{
+    public class Post
+    {
+        public string Title { get;set; }
+
+        public string SubHeading { get; set; }
+
+        public string PreviewBlurb { get; set; }
+
+        public string BlogBody { get; set; }    
+
+
+    }
+}
